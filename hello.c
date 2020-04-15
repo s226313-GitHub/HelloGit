9e6c9e3b4425495f9ddfc6d14e6c1a028a8ef735
s226313-GitHub <63220551+s226313-GitHub@users.noreply.github.com>
2020-04-15 18:29:02 +0900

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : test.c
+ Author      : ‹@ŠB‘¾˜Y(171100)
+ Version     :
+ Copyright   : Copyright (C) 2018 Taro Kikai
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+
+#include<stdio.h>
+
+
+
+int main(int argc, char **argv){
+
+  printf("HelloWorld\n");
+
+
+
+  return 0;
+
+}
